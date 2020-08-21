e9fcaa77a351f397e8baa551e78988bf4687929c
IslamOmar2020 <e.omar_2010@hotmail.com>
2020-08-21 23:51:25 +0300

    first commit

---
+++ Pharmacy/Controllers/Provider/List of All pharmacies/ListofAllpharmaciesVC.swift
@@ -0,0 +1,30 @@
+//
+//  ListofAllpharmaciesVC.swift
+//  Pharmacy
+//
+//  Created by Islam Omar on 8/21/20.
+//  Copyright © 2020 Islam Omar. All rights reserved.
+//
+
+import UIKit
+
+class ListofAllpharmaciesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
